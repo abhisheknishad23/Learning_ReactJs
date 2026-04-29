60a272c585ebc06d27a6c54c05c5161869989ef4
abhisheknishad23 <iabhisheku4@gmail.com>
2026-04-29 17:52:34 +0530

    1

--- first-folder/src/App.jsx
+++ first-folder/src/App.jsx
@@ -1,5 +1,15 @@
-function App(){
-  return 'hello world'
+import React from 'react'
+
+function App() {
+  return (
+    <>
+    <div id='parent'>
+      <h1 id='child1'>hello</h1>
+      <h1 id='child2'>jii</h1>
+    </div>
+    <div id='parent2'></div>
+    </>
+  )
 }
 
 export default App
