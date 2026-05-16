5eb34351e5ce343f5782ba91fd172e9dbfe4c847
abhisheknishad23 <iabhisheku4@gmail.com>
2026-05-16 23:37:49 +0530

    h

---
+++ ui_project/src/components/Section1/Arrow.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Arrow = () => {
+  return (
+    <div className='text-9xl'>
+            <ArrowUpRight />
+        </div>
+  )
+}
+
+export default Arrow
--- ui_project/src/components/Section1/LeftContent.jsx
+++ ui_project/src/components/Section1/LeftContent.jsx
@@ -1,16 +1,14 @@
 import React from 'react'
 import { ArrowUpRight } from 'lucide-react';
+import HeroText from './HeroText';
+import Arrow from './Arrow';
 
 const LeftContent = () => {
   return (
     <div className='h-full flex flex-col justify-between w-1/3'>
-        <div className='p-6'>
-            <h3 className='mb-7 text-7xl font-bold'>Prospective <br></br><span>customer</span><br />segmentation</h3>
-            <p className='text-xl font-medium text-gray-600'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas consectetur alias perspiciatis fugiat quaerat commodi dignissimos, sequi necessitatibus obcaecati eaque.</p>
-        </div>
-        <div className='text-9xl'>
-            <ArrowUpRight />
-        </div>
+        
+        <HeroText />
+        <Arrow />
 
     </div>
   )
