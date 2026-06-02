8b14bace9a37e33cc0ea1410e74a80cf39e8178e
abhisheknishad23 <iabhisheku4@gmail.com>
2026-06-02 21:13:39 +0530

    note app done

--- Note_App_Project/src/App.jsx
+++ Note_App_Project/src/App.jsx
@@ -8,17 +8,27 @@ function App() {
   const [title, setTitle] = useState('')
   const [details, setDetails]=useState('')
 
-  const [task, setTask] = useState('')
+  const [task, setTask] = useState([])
 
 
   const submit = (e)=>{
     e.preventDefault()
-    console.log('form submitted')
-    console.log(title)
-    console.log(details)
+    // console.log('form submitted')
+    // console.log(title)
+    // console.log(details)
+
+    const copyTask=[...task];
+    
+    copyTask.push({title,details})
+    setTask(copyTask)
+    //console.log(task)
 
     setTitle('')
     setDetails('')
+  }
+
+  const deleteNote = ()=>{
+    console.log('deleted')
   }
 
   return (
@@ -32,21 +42,22 @@ function App() {
           value={details} onChange={(e)=>{
             setDetails(e.target.value)
           }}/>
-          <button className='bg-white w-full ouline-none text-black px-5 py-2'>Add Note</button>
+          <button className='bg-white active:bg-amber-600 w-full ouline-none text-black px-5 py-2'>Add Note</button>
       </form>
       <div className='lg:w-1/2 lg:border-l-2 p-10'>
       <h1 className='text-xl font-bold'>Your Notes</h1>
-        <div className='flex flex-wrap gap-5 mt-5 h-full overflow-auto'>
-          <div className='h-52 w-40 rounded-2xl bg-white'></div>
-          <div className='h-52 w-40 rounded-2xl bg-white'></div>
-          <div className='h-52 w-40 rounded-2xl bg-white'></div>
-          <div className='h-52 w-40 rounded-2xl bg-white'></div>
-          <div className='h-52 w-40 rounded-2xl bg-white'></div>
-          <div className='h-52 w-40 rounded-2xl bg-white'></div>
-          <div className='h-52 w-40 rounded-2xl bg-white'></div>
-          <div className='h-52 w-40 rounded-2xl bg-white'></div>
-          <div className='h-52 w-40 rounded-2xl bg-white'></div>
-          <div className='h-52 w-40 rounded-2xl bg-white'></div>
+        <div className='flex flex-wrap items-start justify-start gap-5 mt-5 h-90 overflow-auto'>
+          {task.map(function(elem, idx){
+
+            return <div key={idx} className=" flex justify-between flex-col items-start relative h-52 bg-cover py-8 w-40 rounded-xl p-4 text-black bg-white bg-[url('')]">
+              <div>
+              <h3 className='leading-tight text-xl font-bold'>{elem.title}</h3>
+              <p className='mt-4 leading-tight font-medium text-pink-700'>{elem.details}</p>
+              </div>
+              <button onClick={deleteNote} className='w-full cursor-pointer active:scale-90 bg-amber-800 py-1 text-xs rounded font-bold text-white'>Delete</button>
+            </div>
+            
+          })}
         </div>
       </div>
     </div>
