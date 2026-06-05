47ad29eb053b04e81e2e3c551da1c6afe289bead
abhisheknishad23 <iabhisheku4@gmail.com>
2026-06-05 23:33:20 +0530

    naruto

--- Gallary_App/src/App.jsx
+++ Gallary_App/src/App.jsx
@@ -1,15 +1,18 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const getdata =()=>{
+    console.log('a gya data')
+  }
 
   return (
-    <>
-     
-    </>
+    <div className='bg-black h-screen p-4'>
+      <button onClick={getdata} className='bg-green-400 mt-4 text-white py-2 active:scale-78 px-4 rounded ml-4' >
+        Get Data
+      </button>
+    </div>
   )
 }
 
